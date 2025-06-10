59555efff49492540d2b7e0e4ac57ed81378770e
WolfBeil <84537202+WolfBeil@users.noreply.github.com>
2025-06-10 16:54:15 -0400

    Add basic structure to the tab display modules
    This structure was added to the home-tab.js file.
    An IIFE returning a singleton class is being used.

--- src/home-tab.js
+++ src/home-tab.js
@@ -1,3 +1,17 @@
-export const homeDisplay = (() => {
-    
-})()
+const display = (() => {
+    class display {
+        constructor() {
+            this.mainContainer = document.querySelector('#content');
+        }
+
+        update() {
+            const container = this.mainContainer;
+            container.textContent = '';
+            
+        }
+    }
+
+    return new display();
+})();
+
+export const homeDisplay = display;
