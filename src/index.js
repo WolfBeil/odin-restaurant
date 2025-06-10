e9fdba24bd7fddb3c84b1998865bfdfc266812ef
WolfBeil <84537202+WolfBeil@users.noreply.github.com>
2025-06-10 16:23:14 -0400

    Add display modules files
    All files have been imported to the index.js file.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./styles.css";
+import { homeDisplay } from "./home-tab";
+import { menuDisplay } from "./menu-tab";
+import { aboutDisplay } from "./about-tab";
 
 console.log('Working Correctly');
